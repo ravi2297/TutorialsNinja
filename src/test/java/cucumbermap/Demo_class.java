b69437f790a94d2a37aee5a8b696e8df08a723e7
ravi2297 <ravibhalerao125@gmail.com>
2022-03-19 13:15:31 +0530

    By miss jyoti

---
+++ src/test/java/cucumbermap/Demo_class.java
@@ -0,0 +1,10 @@
+package cucumbermap;
+
+public class Demo_class {
+
+	public static void main(String[] args)
+	{
+		System.out.println("this is demo class to verify git commands are working or not...");
+		
+	}
+}
